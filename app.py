b949b7cc07f98a04f2b464a1d3b850c0701c5152
Pranav Kumar <pranav98@gmail.com>
2025-06-20 13:58:05 -0700

    add frontend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template_string
+from flask import Flask, request, redirect, url_for, render_template
 import sqlite3
 import os
 
@@ -29,17 +29,7 @@ def index():
     c.execute('SELECT name, weight, height, age, medications FROM patients')
     patients = c.fetchall()
     conn.close()
-    return render_template_string('''
-        <h2>All Patients</h2>
-        <a href="{{ url_for('add_patient') }}">Add Patient</a>
-        <ul>
-        {% for p in patients %}
-          <li>{{ p[0] }} | Weight: {{ p[1] }} | Height: {{ p[2] }} | Age: {{ p[3] }} | Medications: {{ p[4] }}</li>
-        {% else %}
-          <li>No patients found.</li>
-        {% endfor %}
-        </ul>
-    ''', patients=patients)
+    return render_template('index.html', patients=patients)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_patient():
@@ -56,18 +46,7 @@ def add_patient():
         conn.commit()
         conn.close()
         return redirect(url_for('index'))
-    return render_template_string('''
-        <h2>Add Patient</h2>
-        <form method="post">
-          Name: <input name="name" required><br>
-          Weight: <input name="weight" type="number" step="any"><br>
-          Height: <input name="height" type="number" step="any"><br>
-          Age: <input name="age" type="number"><br>
-          Medications: <input name="medications"><br>
-          <input type="submit" value="Add Patient">
-        </form>
-        <a href="{{ url_for('index') }}">Back to List</a>
-    ''')
+    return render_template('add_patient.html')
 
 if __name__ == '__main__':
     init_db()
